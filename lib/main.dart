4c904e597615e6aba86e898342227d549e963e9f
isaac <sgpark3@gsenc.com>
2021-04-20 17:32:00 +0900

    build all codes from example

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:speech_recognition/speech_recognition.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,6 +22,51 @@ class VoiceHome extends StatefulWidget {
 }
 
 class _VoiceHomeState extends State<VoiceHome> {
+  SpeechRecognition _speechRecognition;
+  bool _isAvailable = false;
+  bool _isListening = false;
+
+  String resultText = "";
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void initSpeechRecognizer() {
+    _speechRecognition = SpeechRecognition();
+
+    _speechRecognition.setAvailabilityHandler((bool result) {
+      setState(() {
+        _isAvailable = result;
+      });
+    });
+
+    _speechRecognition.setRecognitionStartedHandler(() {
+      setState(() {
+        _isListening = true;
+      });
+    });
+
+    _speechRecognition.setRecognitionResultHandler((String speech) {
+      setState(() {
+        resultText = speech;
+      });
+    });
+
+    _speechRecognition.setRecognitionCompleteHandler(() {
+      setState(() {
+        _isListening = false;
+      });
+    });
+
+    _speechRecognition.activate().then((result) {
+      setState(() {
+        _isAvailable = result;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +74,21 @@ class _VoiceHomeState extends State<VoiceHome> {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text('resultTExt'),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.8,
+          decoration: BoxDecoration(
+            color: Colors.cyanAccent[100],
+            borderRadius: BorderRadius.circular(6.0),
+          ),
+          padding: EdgeInsets.symmetric(
+            vertical: 8.0,
+            horizontal: 12.0,
+          ),
+          child: Text(
+            resultText,
+            style: TextStyle(fontSize: 24.0),
+          ),
+        ),
         SizedBox(
           height: 100,
         ),
@@ -36,7 +96,15 @@ class _VoiceHomeState extends State<VoiceHome> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                if (_isListening)
+                  _speechRecognition.cancel().then(
+                        (result) => setState(() {
+                          _isListening = result;
+                          resultText = "";
+                        }),
+                      );
+              },
               child: Icon(Icons.cancel),
               mini: true,
               backgroundColor: Colors.deepOrange,
@@ -45,14 +113,24 @@ class _VoiceHomeState extends State<VoiceHome> {
               width: 50,
             ),
             FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                if (_isAvailable && !_isListening)
+                  _speechRecognition
+                      .listen(locale: "en_US")
+                      .then((result) => print('$result'));
+              },
               child: Icon(Icons.mic),
             ),
             SizedBox(
               width: 50,
             ),
             FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                if (_isListening)
+                  _speechRecognition.stop().then(
+                        (result) => setState(() => _isListening = result),
+                      );
+              },
               child: Icon(Icons.stop),
               mini: true,
               backgroundColor: Colors.deepPurple,
